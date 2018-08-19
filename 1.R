b816f843ac19d5f754e40f112a254518b8f59c2a
AnishaEkka <anisha.p17003@iimtrichy.ac.in>
2018-08-19 19:52:53 +0530

    19/08/18

--- 1.R
+++ 1.R
@@ -2,3 +2,11 @@ mtcars
 git config --global user.email "anisha.p17003@iimtrichy.ac.in"
 git config --global user.name "AnishaEkka"
 women
+plot(women)
+?women
+?mean
+x= 1:100
+?women
+x= 1:100
+y=seq(1, 100, by=2)
+x
